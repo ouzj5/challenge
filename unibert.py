ddc4de1cc8bfdadbb570e5003a9d9a8e94ff40b6
ouzj5 <953024504@qq.com>
2022-05-31 22:27:14 +0800

    change the aff fusion

--- unibert.py
+++ unibert.py
@@ -61,5 +61,6 @@ class UniBert(BertPreTrainedModel):
 
         embedding_output = torch.cat([text_emb, video_emb], 1)
         mask = torch.cat([text_mask, video_mask], 1)
+        mask = mask[:, None, None, :]
         encoder_outputs = self.encoder(embedding_output, attention_mask=mask)['last_hidden_state']
         return encoder_outputs
